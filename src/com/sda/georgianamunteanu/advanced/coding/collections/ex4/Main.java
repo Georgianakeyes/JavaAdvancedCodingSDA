2dac2f12935071435408f54868e04cda26fcd042
Georgiana Keyes <georgianamkeyes@gmail.com>
2022-11-20 15:32:44 +0200

    ex8

--- src/com/sda/georgianamunteanu/advanced/coding/collections/ex4/Main.java
+++ src/com/sda/georgianamunteanu/advanced/coding/collections/ex4/Main.java
@@ -1,2 +1,39 @@
-package com.sda.georgianamunteanu.advanced.coding.collections.ex4;public class Main {
+package com.sda.georgianamunteanu.advanced.coding.collections.ex4;
+/**
+ * Ex 4
+ * Write a program which creates a concordance of characters occurring in a string (i.e., which characters occur where in a string).
+ * Read the string from the command line.
+ * Ex: {d=[9], o=[4, 6], r=[7], W=[5], H=[0], l=[2, 3, 8], e=[1]}
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getCharactersPosition("Hello World"));
+
+    }
+
+    public static Map<Character, List<Integer>> getCharactersPosition(String input) {
+        Map<Character, List<Integer>> positionByChar = new HashMap<>();
+        for (int i = 0; i < input.toCharArray().length; i++) {
+
+            char currentChar = input.toCharArray()[i];
+            if (positionByChar.containsKey(currentChar)) {
+                List<Integer> positionOfChar = positionByChar.get(currentChar);
+
+
+            } else {
+                List<Integer> positionOfChar = new ArrayList<>();
+                positionOfChar.add(i);
+                positionByChar.put(currentChar, positionOfChar);
+            }
+
+        }
+        return positionByChar;
+
+    }
 }
---
+++ src/com/sda/georgianamunteanu/advanced/coding/collections/ex9/Main.java
@@ -0,0 +1,4 @@
+package com.sda.georgianamunteanu.advanced.coding.collections.ex9;
+
+public class Main {
+}
